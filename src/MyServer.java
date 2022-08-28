035f4108626886a3c71f9b8777ca8e0255a68def
moting <3483608762@qq.com>
2022-08-28 10:26:14 +0800

    bb commit1

--- src/MyServer.java
+++ src/MyServer.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class MyServer{
     public static void main(String[] args) throws IOException {
         System.out.println("冲突");
+        System.out.println("make 冲突");
     }
 
 }
